b7bf9acd3fefeec8d4a83fab858a9ed1bab5297e
Salma AZIZ <aziz.salma095@gmail.com>
2025-11-23 17:56:00 +0100

    first wagtail app - test

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(1p*7z_$$v1xy_1ydgefm_(%nb@-2t_*!bv(s&h#u^!^2ibj%e"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
