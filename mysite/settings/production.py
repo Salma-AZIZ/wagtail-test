9846d6ed52541a303296bb0a36eb79f1a30e2cf2
Salma AZIZ <aziz.salma095@gmail.com>
2025-11-24 22:54:30 +0100

    suppression des settings sur postgreSQL et garder juste SQLite

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -20,12 +20,12 @@ SECRET_KEY = "django-insecure-(1p*7z_$$v1xy_1ydgefm_(%nb@-2t_*!bv(s&h#u^!^2ibj%e
 ALLOWED_HOSTS = ["*"]
 
 
+# === DATABASE : SQLite en Production sur Render ===
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
 }
 
 STATIC_URL = "/static/"
