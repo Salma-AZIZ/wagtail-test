ab3f61952a17530b58171a707c6a780479998dbc
Salma AZIZ <aziz.salma095@gmail.com>
2025-11-24 21:55:17 +0100

    Settings pour deploiement sur Render

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,4 +1,7 @@
 from .base import *
+import os
+import dj_database_url
+
 
 DEBUG = False
 
@@ -9,6 +12,30 @@ DEBUG = False
 STORAGES["staticfiles"][
     "BACKEND"
 ] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(1p*7z_$$v1xy_1ydgefm_(%nb@-2t_*!bv(s&h#u^!^2ibj%e"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 try:
     from .local import *
